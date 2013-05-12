19a3007e819fceb9f8ef70969dfb8af34800ff24
Mateusz Bruno-Kaminski <m.bruno.kaminski@gmail.com>
2013-05-12 14:43:21 +0200

    Pierwsze kody.

---
+++ Code/Visualization/TrafficFlow/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+
+namespace TrafficFlow
+{
+    public static class Program
+    {
+        public static Form1 form;
+
+        [STAThread]
+        public static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            form = new Form1();
+            Application.Run(form);
+        }
+
+        public static void testDraw()
+        {
+            TerrainGraph G = new TerrainGraph();
+            Visualization D = new Visualization();
+            G.addVertex(444, 114 - 22);
+            G.addEdge(0, G.addVertex(384, 250 - 22), "Aleja Trzech Wieszczów");
+            G.addEdge(1, G.addVertex(359, 342 - 22), "Aleja Trzech Wieszczów");
+            G.addEdge(2, G.addVertex(48, 214 - 22), "Czarnowiejska");
+            drawCurve(Color.FromArgb(90, 50, 50, 50), G.streetCurve("Aleja Trzech Wieszczów"));
+            drawCurve(Color.FromArgb(170, 178, 34, 34), G.streetCurve("Czarnowiejska"));
+        }
+
+        public static void drawCurve(Color color, List<Tuple<int, int>> coor)
+        {
+            Brush brush = new SolidBrush(color);
+            Pen pen = new Pen(color, 5);
+            Graphics graphics = form.picture.CreateGraphics();
+            graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            for (int i = 1; i < coor.Count; ++i)
+                graphics.DrawLine(pen, coor[i - 1].Item1, coor[i - 1].Item2, coor[i].Item1, coor[i].Item2);
+            pen.Dispose();
+            graphics.Dispose();
+        }
+    }
+}
---
+++ Code/Visualization/TrafficFlow/TerrainGraph.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrafficFlow
+{
+    public class TerrainGraph
+    {
+        public class StreetInfo
+        {
+            public int it;
+            public int segments;
+
+            public StreetInfo(int i)
+            {
+                it = i; segments = 0;
+            }
+        }
+
+        public static Dictionary<string, StreetInfo> Streets = new Dictionary<string, StreetInfo>()
+        {
+            {"Aleja Trzech Wieszczów", new StreetInfo(0)},
+            {"Czarnowiejska", new StreetInfo(1)}
+        };
+
+        public class Vertex
+        {
+            public int X, Y;
+            public List<Edge> Adj;
+            public bool visited;
+
+            public Vertex(int x, int y)
+            {
+                this.X = x;
+                this.Y = y;
+                this.visited = false;
+                this.Adj = new List<Edge>();
+            }
+        }
+
+        public class Edge
+        {
+            public int street;
+            public int color;
+            public Vertex target;
+
+            public Edge(int s, Vertex u)
+            {
+                street = s;
+                color = 0;
+                target = u;
+            }
+        }
+
+        public List<Vertex> Vertices;
+
+        public TerrainGraph()
+        {
+            this.Vertices = new List<Vertex>();
+        }
+
+        public int addVertex(int x, int y)
+        {
+            this.Vertices.Add(new Vertex(x, y));
+            return this.Vertices.Count - 1;
+        }
+
+        public void addEdge(int u, int v, string street)
+        {
+            this.Vertices[u].Adj.Add(new Edge(Streets[street].it, this.Vertices[v]));
+            Streets[street].segments++;
+        }
+
+        private void visitVertex(Vertex u, int street, List<Tuple<int, int>> result)
+        {
+            foreach (Edge e in u.Adj)
+                if (!e.target.visited)
+                {
+                    if (e.street == street)
+                    {
+                        if (result.Count == 0)
+                            result.Add(new Tuple<int, int>(u.X, u.Y));
+                        result.Add(new Tuple<int, int>(e.target.X, e.target.Y));
+                    }
+                    visitVertex(e.target, street, result);
+                }
+            
+            u.visited = true;
+        }
+
+        private void DFS(int street, List<Tuple<int, int>> result)
+        {
+            foreach (Vertex u in this.Vertices)
+                u.visited = false;
+
+            foreach (Vertex u in this.Vertices)
+                if (!u.visited)
+                    visitVertex(u, street, result);
+        }
+
+        public List<Tuple<int, int>> streetCurve(string street)
+        {
+            List<Tuple<int, int>> result = new List<Tuple<int, int>>();
+            this.DFS(Streets[street].it, result);
+            return result;
+        }
+    }
+}
