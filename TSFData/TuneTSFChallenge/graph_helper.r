617fdfb07f73d8f2449475a47946630e4618557f
kudkudak <staszek.jastrzebski@gmail.com>
2013-05-12 15:41:07 -0400

    Pierwsze kroki z modelem 0b - develop graph_helper.r

--- TSFData/TuneTSFChallenge/graph_helper.r
+++ TSFData/TuneTSFChallenge/graph_helper.r
@@ -1,1 +1,23 @@
+#create_igraph <- fun
+rm(list = ls())
+
+file_nodes = "data/street_graph_nodes.csv" # format jak z OpenStreetMap
+file_edges = "data/street_graph_edges.csv" # format jak z OpenStreetMap
+
+
+nodes = read.csv(file_nodes, header = T, sep= " ")
+edges = read.csv(file_edges, header = T, sep= " ")
+
+
+
+require(igraph)
+library(igraph)
+
+g = graph.empty() 
+g = add.vertices(g, dim(nodes)[1])
+
+V(g)$Latitude = nodes[,"Latitude"]
+V(g)$Longitude = nodes[, "Longitude"]
+
+print(summary(nodes))
 
