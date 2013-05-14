f249511a7d28fbcca4ce75826046292802cbc621
Szymon Nakoneczny <szymon.nakoneczny@gmail.com>
2013-05-14 20:32:30 +0200

    Tworzenie grafu wraz ze slownikami z danych TSF w formacie csv.

---
+++ CityGraph/Testing/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Graph;
+
+namespace Testing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // File path: .../ABBITChallenge/CityGraph/tsf_graph.csv
+            CityGraph testGraph = new CityGraph("D:/Programowanie/ABBITChallenge/CityGraph/tsf_graph.csv");
+            Console.ReadLine();
+        }
+    }
+}
