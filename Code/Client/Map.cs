d3c587cd2000ebad7bcb54aa05e83dd36bc584c2
Mateusz Bruno-Kaminski <m.bruno.kaminski@gmail.com>
2013-05-19 02:13:59 +0200

    Znaczna poprawa wczytywania mapy + rysowanie grafu + samochod

--- Code/Client/Map.cs
+++ Code/Client/Map.cs
@@ -6,6 +6,9 @@ using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.GamerServices;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using ProjNet.Converters;
+using ProjNet.CoordinateSystems;
+using ProjNet.CoordinateSystems.Transformations;
 
 namespace TrafficFlow
 {
@@ -53,6 +56,13 @@ namespace TrafficFlow
             End = y;
             Color = color;
         }
+
+        public Line(Line line)
+        {
+            Start = new Vector2(line.Start.X, line.Start.Y);
+            End = new Vector2(line.End.X, line.End.Y);
+            Color = new Color(line.Color.R, line.Color.G, line.Color.B, line.Color.A);
+        }
     }
     #endregion
 
@@ -62,32 +72,45 @@ namespace TrafficFlow
         public Vector2 Pos;
         public float Velocity;
         public float Time;
-        public bool Finished;
+        public int ZoomCoef;
 
-        private Queue<Tuple<Vector2, Vector2>> Route;
+        private Queue<Line> Route;
         private Vector2 Versor;
         private float CurrentDistance;
         private float DesiredDistance;
 
-        public Car(Queue<Tuple<Vector2, Vector2>> route, float v)
+        public Car(float v, int zoomCoef)
         {
             Pos = new Vector2();
             Velocity = v;
             Time = 0;
-            Finished = false;
-            Route = new Queue<Tuple<Vector2, Vector2>>(route);
+            ZoomCoef = zoomCoef;
+            Route = new Queue<Line>();
             CurrentDistance = 0;
             DesiredDistance = -1;
         }
 
-        public void AddRoute(Queue<Tuple<Vector2, Vector2>> route)
+        public Car(Queue<Line> route, float v, int zoomCoef)
         {
-            Route = new Queue<Tuple<Vector2, Vector2>>(route);
+            Pos = new Vector2();
+            Velocity = v;
+            Time = 0;
+            ZoomCoef = zoomCoef;
+            Route = new Queue<Line>(route);
+            CurrentDistance = 0;
+            DesiredDistance = -1;
         }
 
-        public void AddLine(Tuple<Vector2, Vector2> line)
+        //dodaje całą trasę
+        public void AddRoute(Queue<Line> route)
         {
-            Route.Enqueue(new Tuple<Vector2, Vector2>(line.Item1, line.Item2));
+            Route = new Queue<Line>(route);
+        }
+
+        //dodaje linię do trasy
+        public void AddLine(Line line)
+        {
+            Route.Enqueue(line);
         }
 
         public void InitLine(Vector2 start, Vector2 end)
@@ -103,13 +126,13 @@ namespace TrafficFlow
         {
             if (CurrentDistance < DesiredDistance)
             {
-                Pos += Velocity * Versor * (t - Time);
-                CurrentDistance += Velocity * (t - Time);
+                Pos += Velocity * Versor * (t - Time) * ZoomCoef;
+                CurrentDistance += Velocity * (t - Time) * ZoomCoef;
             }
             else if (Route.Count > 0)
             {
-                Tuple<Vector2, Vector2> tmp = Route.Dequeue();
-                InitLine(tmp.Item1, tmp.Item2);
+                Line tmp = Route.Dequeue();
+                InitLine(tmp.Start, tmp.End);
             }
             
             Time = t;
@@ -126,20 +149,27 @@ namespace TrafficFlow
 
     public partial class Visualization : Microsoft.Xna.Framework.Game
     {
-        //mapa
-        View CurrentView;
-        const int MapBuffer = 6;
-        LinkedList<LinkedList<Tile>> CurrentTiles;
-        Texture2D EmptyTexture;
+        // MAPA W KAFELKACH
+        Texture2D[][][] Tiles;  //wszystkie kafelki
+        View CurrentView;   //obszar odświeżany
+        int MapBuffer;  //wielkość bufora obszaru
+        Texture2D EmptyTexture; //pusta tekstura (czasami w mapie są puste kafelki)
         Camera2d Camera;
+        int CameraPrecision;    //szybkość przesuwania kamery klawiaturą
 
-        //linie ulic
-        private LinkedList<Line> Lines;
+        // ZOOM
+        int ZoomState;  //stan
+        Vector2 ZoomRange;  //zakres stanu (1-3)
+        int FirstAssetName;
 
-        ///zoom
-        private int ZoomState;
-        private Vector2 ZoomRange;
-        private int FirstAssetName;
+
+        // GRAF ULIC
+        CityGraph CityGraph;    //graf wejściowy
+        Vector2 SphericalStart; //punkt startowy dla rysowania w układzie sferycznym
+        Vector2 CartesianCoef;  //współczynnik do konwersji z układu kartezjańskiego na piksele
+        LinkedList<Line> Lines; //zbiór linii ulic
+
+        
 
         //klawiatura
         KeyboardState KeyboardState;
@@ -149,7 +179,6 @@ namespace TrafficFlow
         Vector2 originalMouseState;
 
         //inne
-        Queue<Tuple<Vector2, Vector2>> route;
         Car Car1;
         double Timer;
 
@@ -160,142 +189,98 @@ namespace TrafficFlow
         {
             KeyboardState = Keyboard.GetState();
             KeyboardMovement = Vector2.Zero;
-            const int precision = 10;
 
             if (KeyboardState.IsKeyDown(Keys.Left)) KeyboardMovement.X--;
             if (KeyboardState.IsKeyDown(Keys.Right)) KeyboardMovement.X++;
             if (KeyboardState.IsKeyDown(Keys.Up)) KeyboardMovement.Y--;
             if (KeyboardState.IsKeyDown(Keys.Down)) KeyboardMovement.Y++;
 
-            Camera.Pos += KeyboardMovement * precision;
+            Camera.Pos += KeyboardMovement * CameraPrecision;
+            Debug.Text = CurrentView.Top + " " + CurrentView.Bottom;
         }
 
         //wiadomo
         private void MouseUpdate()
         {
-            Vector2 currentMouseState = new Vector2(Mouse.GetState().X + Window.ClientBounds.Left, Mouse.GetState().Y + Window.ClientBounds.Top);
-            if (currentMouseState != originalMouseState)
-                Debug.Text = (currentMouseState.X - originalMouseState.X) + " " + (currentMouseState.Y - originalMouseState.Y);
+            Vector2 currentMouseState = new Vector2(Mouse.GetState().X + Camera.Pos.X, Mouse.GetState().Y + Camera.Pos.Y);
+            //if (currentMouseState != originalMouseState)
+            //    Debug.Text = (currentMouseState.X - originalMouseState.X) + " " + (currentMouseState.Y - originalMouseState.Y);
         }
 
-        //ładuje kafelki mapy
-        private void LoadTiles()
+        //aktualizuje granice obszaru dla widocznych kafelek
+        private void ViewUpdate()
         {
-            CurrentTiles.Clear();
-            for (int i = CurrentView.Top; i < MapBuffer + CurrentView.Top; ++i)
+            const int precision = 100;  //255 max
+            if (CurrentView.Right + 1 < 5 * (int)Math.Pow(2, ZoomState - 1) && Camera.Pos.X + Window.ClientBounds.Width > (CurrentView.Right + 1) * 256 - precision)   //prawo
             {
-                CurrentTiles.AddLast(new LinkedList<Tile>());
-                for (int j = CurrentView.Left; j < MapBuffer + CurrentView.Left; ++j)
-                {
-                    try
-                    {
-                        CurrentTiles.Last.Value.AddLast(new Tile(i * 256, j * 256, Content.Load<Texture2D>(@"Tiles\L" + ZoomState + @"\" + i + @"\" + (j + Math.Pow(2, ZoomState - 1) * 1346))));
-                    }
-                    catch (ContentLoadException e)
-                    {
-                        CurrentTiles.Last.Value.AddLast(new Tile(i * 256, j * 256, EmptyTexture));
-                    }
-                }
-            }
-        }
-
-        //aktualizuje kafelki mapy
-        private void TilesUpdate()
-        {
-            const int precision = 200;  //255 max
-            Texture2D texture;
-            if (CurrentView.Right + 1 < 10 && Camera.Pos.X + Window.ClientBounds.Width > (CurrentView.Right + 1) * 256 - precision)   //prawo
-            {
-                CurrentTiles.AddLast(new LinkedList<Tile>());
-                for (int i = CurrentView.Top; i < MapBuffer + CurrentView.Top; ++i)
-                {
-                    try
-                    {
-                        texture = Content.Load<Texture2D>(@"Tiles\L" + ZoomState + @"\" + (CurrentView.Right + 1).ToString() + @"\" + (i + Math.Pow(2, ZoomState - 1) * 1346).ToString());
-                        CurrentTiles.Last.Value.AddLast(new Tile(((int)CurrentView.Right + 1) * 256, i * 256, texture));
-                    }
-                    catch (ContentLoadException e)
-                    {
-                        CurrentTiles.Last.Value.AddLast(new Tile(((int)CurrentView.Right + 1) * 256, i * 256, EmptyTexture));
-                    }
-                }
-
-                CurrentTiles.RemoveFirst();
-
                 ++CurrentView.Left;
                 ++CurrentView.Right;
             }
-            if (CurrentView.Left * 256 > 0 && Camera.Pos.X < CurrentView.Left * 256 + precision)     //lewo
+            if (CurrentView.Left > 0 && Camera.Pos.X < CurrentView.Left * 256 + precision)     //lewo
             {
-                CurrentTiles.AddFirst(new LinkedList<Tile>());
-                for (int i = CurrentView.Top; i < MapBuffer + CurrentView.Top; ++i)
-                {
-                    try
-                    {
-                        texture = Content.Load<Texture2D>(@"Tiles\L" + ZoomState + @"\" + (CurrentView.Left - 1).ToString() + @"\" + (i + Math.Pow(2, ZoomState - 1) * 1346).ToString());
-                        CurrentTiles.First.Value.AddFirst(new Tile(((int)CurrentView.Left - 1) * 256, i * 256, texture));
-                    }
-                    catch (ContentLoadException e)
-                    {
-                        CurrentTiles.First.Value.AddFirst(new Tile(((int)CurrentView.Left - 1) * 256, i * 256, EmptyTexture));
-                    }
-                }
-
-                CurrentTiles.RemoveLast();
-
                 --CurrentView.Left;
                 --CurrentView.Right;
             }
-            if (CurrentView.Bottom + 1 < 10 && Camera.Pos.Y + Window.ClientBounds.Height > (CurrentView.Bottom + 1) * 256 - precision)   //dół
+            if (CurrentView.Bottom + 1 < 3 * (int)Math.Pow(2, ZoomState - 1) && Camera.Pos.Y + Window.ClientBounds.Height > (CurrentView.Bottom + 1) * 256 - precision)   //dół
             {
-                LinkedListNode<LinkedList<Tile>> list = CurrentTiles.First;
-                for (int i = CurrentView.Left; i < MapBuffer + CurrentView.Left; ++i)
-                {
-                    try
-                    {
-                        texture = Content.Load<Texture2D>(@"Tiles\L" + ZoomState + @"\" + i.ToString() + @"\" + (CurrentView.Bottom + 1 + Math.Pow(2, ZoomState - 1) * 1346).ToString());
-                        list.Value.AddLast(new Tile(i * 256, ((int)CurrentView.Bottom + 1) * 256, texture));
-                    }
-                    catch (ContentLoadException e)
-                    {
-                        list.Value.AddLast(new Tile(i * 256, ((int)CurrentView.Bottom + 1) * 256, EmptyTexture));
-                    }
-
-                    list.Value.RemoveFirst();
-                    list = list.Next;
-                }
-
                 ++CurrentView.Top;
                 ++CurrentView.Bottom;
             }
-            if (CurrentView.Top * 256 > 0 && Camera.Pos.Y < CurrentView.Top * 256 + precision)   //góra
+            if (CurrentView.Top > 0 && Camera.Pos.Y < CurrentView.Top * 256 + precision)   //góra
             {
-                LinkedListNode<LinkedList<Tile>> list = CurrentTiles.First;
-                for (int i = CurrentView.Left; i < MapBuffer + CurrentView.Left; ++i)
-                {
-                    try
-                    {
-                        texture = Content.Load<Texture2D>(@"Tiles\L" + ZoomState + @"\" + i.ToString() + @"\" + (CurrentView.Top - 1 + Math.Pow(2, ZoomState - 1) * 1346).ToString());
-                        list.Value.AddFirst(new Tile(i * 256, ((int)CurrentView.Top - 1) * 256, texture));
-                    }
-                    catch (ContentLoadException e)
-                    {
-                        list.Value.AddFirst(new Tile(i * 256, ((int)CurrentView.Top - 1) * 256, EmptyTexture));
-                    }
-
-                    list.Value.RemoveLast();
-                    list = list.Next;
-                }
-
                 --CurrentView.Top;
                 --CurrentView.Bottom;
             }
         }
 
-        //dodaj linię do listy
-        private void AddLine(Vector2 x, Vector2 y, Color color)
+        //ogarnia stan zoomu
+        private void ZoomUpdate(bool change)
         {
-            Lines.AddLast(new Line(x, y, color));
+            if (change && ZoomState < ZoomRange.Y)
+                ++ZoomState;
+            else if (!change && ZoomState > ZoomRange.X)
+                --ZoomState;
+            else
+                return;
+
+            MapBuffer = 5;
+            int worldWidth = 5 * (int)Math.Pow(2, ZoomState - 1) * 256 - Window.ClientBounds.Width;
+            int worldHeight = 3 * (int)Math.Pow(2, ZoomState - 1) * 256 - Window.ClientBounds.Height;
+            Camera.Reinitialize(worldWidth, worldHeight);
+        }
+
+        //dodaje linię do listy
+        private void AddLine(Line l)
+        {
+            Line line = new Line(l);
+            Lines.AddLast(line);
+        }
+
+        //przyjmuje współrzędne geograficzne i zwraca kartezjańskie
+        private Vector2 CoordinateTransformation(double[] spherical)
+        {
+            CoordinateTransformationFactory ctfac = new CoordinateTransformationFactory();
+            ICoordinateTransformation trans = ctfac.CreateFromCoordinateSystems(GeographicCoordinateSystem.WGS84, ProjectedCoordinateSystem.WGS84_UTM(34, true));
+
+            double[] point = new double[] { spherical[0], spherical[1] };
+            double[] cartesian = trans.MathTransform.Transform(point);
+            double[] result = new double[2] { cartesian[0] - SphericalStart.X, SphericalStart.Y - cartesian[1] };
+            return new Vector2((float)result[0] / CartesianCoef.X, (float)result[1] / CartesianCoef.Y);
+        }
+
+        //tworzy sieć ulic
+        private void BuildNetwork()
+        {
+            CityGraph = new CityGraph("./warsaw_graph.xml");
+            Vector2 start, end;
+            int count = 0;
+            foreach (MyEdge e in CityGraph.graph.Edges)
+            {
+                start = CoordinateTransformation(new double[2]{e.startNode.longitude, e.startNode.latitude});
+                end = CoordinateTransformation(new double[2]{e.endNode.longitude, e.endNode.latitude});
+                if(start.X >= 0 && start.Y >= 0 && end.X <= 1024 && end.Y <= 1024)
+                    AddLine(new Line(new Vector2(start.X, start.Y), new Vector2(end.X, end.Y), new Color(100, 100, 100, 255)));
+            }
         }
     }
 }
